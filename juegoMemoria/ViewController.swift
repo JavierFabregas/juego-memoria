fa850d00f46316abfdeb566bc7e9523caafbb62b
javier <javier_fabregas_apps1ma1819@cev.com>
2019-10-15 10:26:39 +0200

    sin cambios

--- juegoMemoria/ViewController.swift
+++ juegoMemoria/ViewController.swift
@@ -6,11 +6,12 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var cadroImagen: UIImageView!
     @IBOutlet weak var numeroImagen: UILabel!
-    
+    var imagenes: [UIImage] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
     
 
 }
